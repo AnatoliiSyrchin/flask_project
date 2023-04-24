9ba7e72b7dfe928d72331c95e836784a4cf49cae
tolik <9779930@mail.ru>
2023-04-24 08:07:19 +0300

    auth_app

--- blog/app.py
+++ blog/app.py
@@ -3,19 +3,25 @@ from flask import Flask
 from blog.users.views import users_app
 from blog.articles.views import articles_app
 from blog.models.datebase import db
+from blog.auth.view import auth_app, login_manager
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
 
+    app.config["SECRET_KEY"] = "abcdefg123456"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     db.init_app(app)
+    login_manager.init_app(app)
 
     register_blueprints(app)
+
     return app
 
 
 def register_blueprints(app: Flask):
     app.register_blueprint(users_app)
     app.register_blueprint(articles_app)
+    app.register_blueprint(auth_app)
