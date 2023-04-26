cb7b6d7afd84ef9dac009dc122d93eadfb810f3f
Tolik <99863916+AnatoliiSyrchin@users.noreply.github.com>
2023-04-26 20:25:24 +0300

    Lesson 3 (#3)
    * datebase
    
    * auth_app
    
    * added password and auth check

---
+++ blog/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from blog.models.datebase import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(Integer, primary_key=True)
+    username = db.Column(String(256), unique=True, nullable=False)
+    password = db.Column(String(256), unique=True, nullable=False)
+    is_staff = db.Column(Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return f'User {self.username}'
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,37 @@
 from blog.app import create_app
 from flask import render_template
+from blog.models.datebase import db
+from werkzeug.security import generate_password_hash
+
 
 app = create_app()
+
+
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in your terminal:
+    flask init-db
+    """
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    """
+    Run in your terminal:
+    flask create-users
+    > done! created users: <User #1 'admin'> <User #2 'james'>
+    """
+    from blog.models import User
+    admin = User(username="admin", password=generate_password_hash('111'), is_staff=True)
+    james = User(username="james", password=generate_password_hash('222'))
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+    print("done! created users:", admin, james)
+
 
 @app.route('/')
 def index():
