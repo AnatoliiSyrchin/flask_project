8c65eac56458f64b6911e7a331bcfc90f7c969d9
Tolik <99863916+AnatoliiSyrchin@users.noreply.github.com>
2023-05-02 20:10:29 +0300

    WTForms (#5)

--- blog/config.py
+++ blog/config.py
@@ -1,14 +1,15 @@
 import os
 
 class BaseConfig(object):
-    DEBUG = False
+    DEBUG = True
     TESTING = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///blog.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "xk@(!uj(68$86abj_93uowhgo-5sosju&_x%6j=^xm*=v2!9kc"
+    WTF_CSRF_ENABLED = True
 
 class DevConfig(BaseConfig):
-    DEBUG = True
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
 class TestingConfig(BaseConfig):
--- blog/models/users.py
+++ blog/models/users.py
@@ -1,15 +1,29 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Text
 from blog.models.datebase import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
-    email = Column(String(255), nullable=False, default="", server_default="")
+    email = Column(String(255), nullable=False, unique=True, default="", server_default="")
     username = Column(String(256), unique=True, nullable=False)
-    password = Column(String(256), unique=True, nullable=False)
     is_staff = Column(Boolean, nullable=False, default=False)
 
+    first_name = Column(String(255), unique=False, nullable=False, default='', server_default='')
+    last_name = Column(String(255), unique=False, nullable=False, default='', server_default='')
+    _password = Column(Text, nullable=True)
+
+    @property
+    def password(self):
+        return self._password
+    
+    @password.setter
+    def password(self, value):
+        self._password = generate_password_hash(value)
+
+    def validate_password(self, password) -> bool:
+        return check_password_hash(self._password, password)
 
     def __repr__(self):
         return f'User {self.username}'
